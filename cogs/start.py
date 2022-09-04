d2766c7c9adfbeabad5f8676112617295ed756f2
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-04 17:12:28 +0200

    Update start.py

--- cogs/start.py
+++ cogs/start.py
@@ -1,1 +1,34 @@
+import os
+import json
+import pymongo
+import discord
+from discord.ext import commands
+from datetime import datetime
 
+configfile = os.path.join(os.path.dirname(__file__))
+configfile = configfile[:-4]
+configfile = f'{configfile}config.json'
+
+with open(configfile, 'r') as f:
+    config = json.load(f)
+
+prf = config["cmd_prefix"]
+
+# cluster = pymongo.MongoClient("mongodb+srv://bebroidik:okBL9W7DABgfpqhf@cluster0.7wcrgyr.mongodb.net/?retryWrites=true&w=majority")
+# db = cluster["cluster0"] # название кластера, не меняй его
+# collection = db["uptime"] # название колекции, не меняй его
+
+class Start(commands.Cog):    
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        # dtint = datetime.timestamp(datetime.now())
+        # collection.update_one({"_id": 0}, {"$set": {"uptime": int(dtint)}})
+        botping = int(self.bot.latency * 1000)
+        await self.bot.change_presence(activity=discord.Game(name=f"{prf}"), status = discord.Status.dnd)
+        print(f'{self.bot.user} | {self.bot.user.id} запущен\n\nКоличество выгруженных файлов: {len(self.bot.extensions)}, когов: {len(self.bot.cogs)}\nПинг бота: {botping} мс\n------------------------')
+        
+def setup(bot):
+    bot.add_cog(Start(bot))
