4268c1829acc3d55d5b30946a0ea371e7ece749f
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-04 22:43:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ import config
 import discord
 from discord.ext import commands
 
+
+class MyContext(commands.Context):  
+    async def error(self, *, description = None, fields = None):
+        emberror = discord.Embed(
+            title = '❌ Ошибка',
+            description = description
+            fields = fields
+        )
+        emberror.set_footer(text = message.author, icon_url = message.author.display_avatar.url)
+        try:
+            await self.message.reply(embed = emberror)
+
+
+class MyBot(commands.Bot):
+    async def get_context(self, message: discord.Message, *, cls = MyContext):
+        return await super().get_context(message, cls = cls)
+   
+
 bot_intents = discord.Intents.all()
 
-my_bot = commands.Bot(
+my_bot = MyBot(
     intents = bot_intents, 
     command_prefix = commands.when_mentioned_or(config.cmd_prefix)
 )
-
 my_bot.remove_command('help')
 
 for filename in os.listdir('./cogs'):
