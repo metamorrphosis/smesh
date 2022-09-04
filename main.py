ee5669eecd44efb02b73fc25c69830cc6031a70c
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-04 21:09:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 import os
 import json
+import config
 import discord
 from discord.ext import commands
-
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-cmd_prefix = config["cmd_prefix"]
-token_main = config["token_main"]
 
 bot_intents = discord.Intents.all()
 
 my_bot = commands.Bot(
     intents = bot_intents, 
-    command_prefix = commands.when_mentioned_or(cmd_prefix)
+    command_prefix = commands.when_mentioned_or(config.cmd_prefix)
 )
 
 my_bot.remove_command('help')
 
 for filename in os.listdir('./cogs'):
-    if filename.endswith('.py') and filename != 'utils.py':
+    if filename.endswith('.py'):
         my_bot.load_extension(f'cogs.{filename[:-3]}')
         print(f'"{filename[:-3]}" загружен')
 
 
-my_bot.run(token_main)
+my_bot.run(config.token_main)
