9f8c7cff31d22b04ef246555f521cc7ce677bc05
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-04 23:31:29 +0200

    Update tickets.py

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -1,18 +1,26 @@
 import discord
 from utils import database
 from discord.ext import commands
-# <:asm_stormy_curator:1001817272240844911> 
+
+
+class StartTicketView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout = None)
+    
+    @discord.ui.button(
+        emoji = discord.PartialEmoji.from_str('<:asm_stormy_curator:1001817272240844911>'), 
+        style = discord.ButtonStyle.green,
+        custom_id = "open_ticket",
+        label = 'Открыть тикет'
+    )
+    async def callback(self, button, interaction):
+        pass
 
 
 class TicketsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.mention_message = '<@&991219359731163187> <@&989892564691873793> <@&1009021230080348190> <@&989891381575159870>'
-        self.guild = bot.get_guild(837941760193724426)
-        # self.staff_role = self.guild.get_role(991219359731163187)
-        # self.helper_role = self.guild.get_role(989892564691873793)
-        # self.support_role = self.guild.get_role(1009021230080348190)
-        #self.moder_role = self.guild.get_role(989891381575159870)
     
     @commands.command()
     @commands.guild_only()
@@ -24,7 +32,7 @@ class TicketsCog(commands.Cog):
             color = 0xbffed9
         )
         await ctx.message.delete()
-        await ctx.send(embed = embticket)
+        await ctx.send(embed = embticket, view = StartTicketView())
     
     @commands.command()
     @commands.guild_only()
@@ -35,9 +43,13 @@ class TicketsCog(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
-        self.b = self.bot.get_guild(837941760193724426)
-        
-        print(self.bot.get_guild(837941760193724426), self.mention_message)
+        self.smesh_guild = self.bot.get_guild(837941760193724426)
+        self.staff_role = self.smesh_guild.get_role(991219359731163187)
+        self.helper_role = self.smesh_guild.get_role(989892564691873793)
+        self.support_role = self.smesh_guild.get_role(1009021230080348190)
+        self.moder_role = self.smesh_guild.get_role(989891381575159870)
+        self.bot.add_view(StartTicketView())
+    
     
 
 def setup(bot):
