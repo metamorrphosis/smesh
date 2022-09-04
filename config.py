11168ac7fe216a448f442d492506581de3829b2b
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-04 20:53:06 +0200

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+token_main = "MTAxNTk4MzAwNjU0NjE0OTQyNw.GunmkN.6tvHYcmmMfMeSt8afqLO7tSL3Gn72cairEtzHM"
+cmd_prefix =  "s."
+mongodb_link = "mongodb+srv://bebroid:G4OkpALTJyAe2rlG@smeshcluster.hmfuskg.mongodb.net/?retryWrites=true&w=majority"
