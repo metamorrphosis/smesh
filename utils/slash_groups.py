27858af9a11ff7d67b74fb7d3683010daf79b8ad
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-11 14:34:33 +0300

    f

--- utils/slash_groups.py
+++ utils/slash_groups.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
+from discord.commands import SlashCommandGroup
 
-tickets_group = commands.SlashCommandGroup(
+tickets_group = SlashCommandGroup(
     name = 'ticket', 
     description = 'Взаимодействие с тикетами', 
     guild_ids = [837941760193724426], 
