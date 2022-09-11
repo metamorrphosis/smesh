8012a6dcebc153b762ebe6f7a679169706ab9d0d
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-11 18:17:20 +0200

    Update vacancies.py

--- cogs/vacancies.py
+++ cogs/vacancies.py
@@ -1,3 +1,4 @@
+import aiohttp
 import discord
 from discord.ext import commands
 from datetime import datetime, timezone
@@ -55,7 +56,7 @@ class HelperModal(discord.ui.Modal):
         timejoin = int(interaction.user.joined_at.replace(tzinfo=timezone.utc).timestamp())
 
         embed = discord.Embed(
-            title="Новая анкета на помощника ",
+            title="Новая анкета на помощника в дискорде",
             fields=[
                 discord.EmbedField(
                     name = "Автор анкеты", 
@@ -101,38 +102,171 @@ class HelperModal(discord.ui.Modal):
         await interaction.response.send_message('Ваша заявка успешно отправлена администрации', ephemeral = True)
 
 
-class VacancyView(discord.ui.View):
+class TelegramModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(
+            discord.ui.InputText(
+                label="Ваше имя",
+                placeholder="Пример: Петя",
+                min_length=4,
+                max_length=100,
+                style=discord.InputTextStyle.short,
+                required=True,
+            ),
+            discord.ui.InputText(
+                label="Ваш часовой пояс",
+                placeholder="Пример: МСК",
+                min_length=1,
+                max_length=100,
+                style=discord.InputTextStyle.short,
+                required=True,
+            ),
+            discord.ui.InputText(
+                label="Ваш телеграмм",
+                placeholder="Пример: @petya228",
+                min_length=4,
+                max_length=150,
+                style=discord.InputTextStyle.long,
+                required=True,
+            ),
+            discord.ui.InputText(
+                label="Был ли у Вас опыт в подобной сфере?",
+                placeholder="Пример: Да был, в телеграмм канале (23,000 участников)",
+                min_length= 20,
+                max_length=500,
+                style=discord.InputTextStyle.long,
+                required=True,
+            ),
+            discord.ui.InputText(
+                label="Сколько времени Вы сможете уделять телеграмму",
+                placeholder="Пример: Два-три часа в день",
+                min_length=2,
+                max_length=500,
+                style=discord.InputTextStyle.long,
+                required=True,
+            ),
+            *args,
+            **kwargs,
+        )
+
+    async def callback(self, interaction):
+        timereg = int(interaction.user.created_at.replace(tzinfo=timezone.utc).timestamp())
+        timejoin = int(interaction.user.joined_at.replace(tzinfo=timezone.utc).timestamp())
+
+        embed = discord.Embed(
+            title="Новая анкета на модератора телеграмм",
+            fields=[
+                discord.EmbedField(
+                    name = "Автор анкеты", 
+                    value = f"{interaction.user.mention} | {interaction.user} | {interaction.user.id}", 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = "Информация по автору анкеты", 
+                    value = f'Дата регистрации аккаунта: <t:{timereg}>\nДата присоединения на сервер: <t:{timejoin}>', 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = self.children[0].label, 
+                    value = self.children[0].value, 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = self.children[1].label, 
+                    value = self.children[1].value, 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = self.children[2].label, 
+                    value = self.children[2].value, 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = self.children[3].label, 
+                    value = self.children[3].value, 
+                    inline = False
+                ),
+                discord.EmbedField(
+                    name = self.children[4].label, 
+                    value = self.children[4].value, 
+                    inline = False
+                ),
+            ],
+            color = 0x2e3133,
+            timestamp = datetime.now()
+        )
+        channel = interaction.guild.get_channel(1007118729697562715)
+        await channel.send(embed = embed)
+        await interaction.response.send_message('Ваша заявка успешно отправлена администрации', ephemeral = True)
+
+
+class HelperView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout = None)
      
     @discord.ui.button(
-        emoji = discord.PartialEmoji.from_str('<:asm_stormy_curator:1001817272240844911>'), 
-        label = 'Заявка на помощника', 
+        emoji = discord.PartialEmoji.from_str('<:asm_stormy_curator:1018512377634893874>'), 
+        label = 'Заявка на помощника в дискорде', 
         style = discord.ButtonStyle.gray, 
         custom_id = "helper_modal"
     )
     async def helper(self, button, interaction):
-        await interaction.response.send_modal(HelperModal(title = 'Заявка на помощника'))
+        await interaction.response.send_modal(HelperModal(title = 'Заявка на роль помощника в дискорд'))
+
+
+class TelegramView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout = None)
+     
+    @discord.ui.button(
+        emoji = discord.PartialEmoji.from_str('<:asm_stormy_moder:1018512445498720316>'), 
+        label = 'Заявка на модератора телеграмм', 
+        style = discord.ButtonStyle.gray, 
+        custom_id = "telegram_modal"
+    )
+    async def helper(self, button, interaction):
+        await interaction.response.send_modal(TelegramModal(title = 'Заявка на роль модератора в телеграмм'))
+
 
 class Vacancies(commands.Cog):    
     def __init__(self, bot):
         self.bot = bot
+        self.hook_url = 'https://discord.com/api/webhooks/1018488382059462726/1X5vAr1FqtsgqNs2KgKUTDKeTS46_99CVapek6R_yyHeerwYU8U_cHf8tveRqrWm2Fzi'
     
     @commands.command()
     @commands.guild_only()
     @commands.has_guild_permissions( administrator = True)
     async def vcs(self, ctx):
-        embvcs = discord.Embed(
-            title = 'Требования для подачи заявки на роль Помощника',
-            description = '──────── [ <:asm_storm_admin:1001811084333830174> ] ────────\n**Наши требования:**\n> <:asm_stormy_5:1001811300399185940>Не менее 14 полных лет\n> <:asm_stormy_5:1001811300399185940>Знание правил сервера\n> <:asm_stormy_5:1001811300399185940>Находится на сервере не менее 2-ух недель\n> <:asm_stormy_5:1001811300399185940>Минимум 5 уровень в `JuniperBot`\n──────── [ <:asm_storm_admin:1001811084333830174> ] ────────\n**Что мы можем предложить взамен:**\n> <:asm_stormy_5:1001811300399185940>Игровая валюта <:vajno_2:996395333322023032>\n\nДля подачи заявки используйте кнопку ниже',
-            color = 0x2e3133
-        )
+        embhelper = [
+            discord.Embed(
+                color = 0xbffed9,
+                image = 'https://media.discordapp.net/attachments/1017458641537859604/1018507896327249961/1662901405981.png'
+            ),
+            discord.Embed(
+                title = '<:emoji_3:1015871727101816852>・Набор на роль Помощника в дискорд',
+                description = '━────────────────━\n**Требования для подачи заявки:**\n<:emoji_1:1015862630453354536>Не менее 14 полных лет.\n<:emoji_2:1015862660497154089>Знание правил сервера.\n<:emoji_1:1015862630453354536>10 уровень в JuniperBot.\n<:emoji_2:1015862660497154089>Опыт работы в подобной сфере.\n\n**Что мы можем предложить взамен:**\n<:emoji_1:1015862630453354536>Зарплата игровой валютой в виде <:vajno_2:1018512718585679882>.',
+                color = 0xbffed9,
+                image = 'https://cdn.discordapp.com/attachments/1017458641537859604/1018492145335816192/SAVE_20220710_205848.jpg'
+            )
+        ]
+        embtg = [
+            discord.Embed(
+                title = '<:emoji_3:1015871727101816852>・Набор на роль Модератора в телеграмм.',
+                description = '━────────────────━\n**Требования для подачи заявки:**\n<:emoji_1:1015862630453354536>Не менее 14 полных лет.\n<:emoji_2:1015862660497154089>Знание правил в телеграмм группе.\n<:emoji_1:1015862630453354536>Активное появление в телеграмме.\n\n**Что мы можем предложить взамен:**\n<:emoji_2:1015862660497154089>Опыт работы в подобной сфере.\n<:emoji_1:1015862630453354536>Новые знакомства и коллектив.',
+                color = 0xbffed9,
+                image = 'https://cdn.discordapp.com/attachments/1017458641537859604/1018492145335816192/SAVE_20220710_205848.jpg'
+            )
+        ]
         await ctx.message.delete()
-        await ctx.send(embed = embvcs, view = VacancyView())
+        async with aiohttp.ClientSession() as session:
+            webhook = discord.Webhook.from_url(self.hook_url, session = session)
+            await webhook.send(embeds = embhelper, view = HelperView())
+            await webhook.send(embeds = embtg, view = TelegramView())
 
     @commands.Cog.listener()
     async def on_ready(self):
-        self.bot.add_view(VacancyView())
+        self.bot.add_view(HelperView())
+        self.bot.add_view(TelegramView())
 
 
 def setup(bot):
