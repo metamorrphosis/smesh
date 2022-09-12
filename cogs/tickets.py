4c96e048a6393098f3cd6bdcb2688261269c4bae
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-12 23:23:03 +0300

    в

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -127,6 +127,8 @@ class TicketsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.db = tickets_db.TicketsDB()
+
+    slash_group = discord.SlashCommandGroup(name = 'ticket', guild_only = True, guild_ids = [837941760193724426])
     
     @commands.command()
     @commands.guild_only()
@@ -140,7 +142,7 @@ class TicketsCog(commands.Cog):
         await ctx.message.delete()
         await ctx.send(embed = embticket, view = StartTicketView())
     
-    @commands.slash_command(name = 'close', description = 'Закрывает тикет', guild_only = True, guild_ids = [837941760193724426])
+    @slash_group.slash_command(name = 'close', description = 'Закрывает тикет', guild_only = True, guild_ids = [837941760193724426])
     async def slash_ticket_close(self, ctx):
         uroles = my_roles.Roles(ctx.guild)
         staff_roles = uroles.get_all_staff_roles()
@@ -164,7 +166,7 @@ class TicketsCog(commands.Cog):
 
         await ctx.send_response('Тикет закрыт')
     
-    @commands.slash_command(name = 'claim', description = 'Принимает тикет', guild_only = True, guild_ids = [837941760193724426])
+    @slash_group.slash_command(name = 'claim', description = 'Принимает тикет', guild_only = True, guild_ids = [837941760193724426])
     async def slash_ticket_claim(self, ctx):
         uroles = my_roles.Roles(ctx.guild)
         staff_roles = uroles.get_all_staff_roles()
