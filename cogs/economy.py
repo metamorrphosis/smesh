d140674d95b857c1b353d1074efdca1fc8df7d78
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-14 23:32:41 +0300

    d

--- cogs/economy.py
+++ cogs/economy.py
@@ -8,9 +8,29 @@ class EconomyCog(commands.Cog):
         self.db = economy_db.EconomyDB()
 
     @commands.command(aliases = ['money', 'bal', 'бал', 'баланс'])
-    async def balance(self, ctx):
-        member_bal = await self.db.get_money(member = ctx.author)
-        print(member_bal)
+    async def balance(self, ctx, member: discord.Member = None):
+        member = member or ctx.author
+        member_bal = await self.db.get_money(member = member)
+        await ctx.natural(
+            title = f'Баланс {member}',
+            fields = [
+                discord.EmbedField(
+                    name = 'Наличные', 
+                    value = f'<:vajno_2:1018512718585679882>{member_bal["cash"]}', 
+                    inline = True
+                ),
+                discord.EmbedField(
+                    name = 'Банк', 
+                    value = f'<:vajno_2:1018512718585679882>{member_bal["bank"]}', 
+                    inline = True
+                ),
+                discord.EmbedField(
+                    name = 'Всего', 
+                    value = f'<:vajno_2:1018512718585679882>{member_bal["cash"] + member_bal["bank"]}', 
+                    inline = True
+                )
+            ]
+        )
         
     
 def setup(bot):
