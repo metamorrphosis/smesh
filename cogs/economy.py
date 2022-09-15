a0a49c20f2c8de59800c7a0367df1609763b38c5
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-15 18:43:55 +0300

    h

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,6 +1,10 @@
+import config
 import discord
+from typing import Union
 from utils import economy_db
 from discord.ext import commands
+prf = config.cmd_prefix
+
 
 class EconomyCog(commands.Cog):
     def __init__(self, bot):
@@ -31,6 +35,21 @@ class EconomyCog(commands.Cog):
                 )
             ]
         )
+    
+    @commands.command(aliases = ['add-money', 'выдать-деньги', 'выдатьденьги', 'аддмоней', 'монейадд'])
+    async def addmoney(self, ctx, member: discord.Member = None, mode: str, value: str = None):
+        usage_field = discord.EmbedField(
+            name = 'Использование команды',
+            value = f'`{prf}add-money <ник, упоминание или ID участника> <куда выдать (наличные, банк), если указать тут не режим а число — выдача в банк> [количество, указывать только если указан режим]`',
+        )
+        examples_field = discord.EmbedField(
+            name = 'Примеры использования команды',
+            value = f'`{prf}add-money @Петя228 5000` — поскольку вместо режима число — выдаст пете 5000 валюты в банк\
+            `{prf}add-money 1007615585506566205 cash 10,000` — выдаст участнику по ID 10,000 валюты в наличные\'
+        )
+        
+        if member is None:
+            return await ctx.error(description = 'Вы не указали участника, которому необходимо выдать валюту', fields = [usage_field, examples_field])
         
     
 def setup(bot):
