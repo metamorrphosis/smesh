cd45556191d243b655bbdf1bd60eb1c30ef19abf
f1z1x <104532312+f1z1x@users.noreply.github.com>
2022-09-16 22:47:33 +0300

    s

--- cogs/temp_warns.py
+++ cogs/temp_warns.py
@@ -1,7 +1,7 @@
 import config
 import discord
 from typing import Union
-from utils import temp_warns_db, my_roles
+from utils import temp_warns_db, my_roles, get_duration
 from utils.other import nc
 from discord.ext import commands
 prf = config.cmd_prefix
@@ -42,7 +42,7 @@ class TempWarnsCog(commands.Cog):
     
 
     @commands.command(aliases = ['устный'])
-    async def temp_warn(self, ctx):
+    async def temp_warn(self, ctx, member: Union[discord.Member, str] = None, duration = None):
         uroles = my_roles.Roles(ctx.guild)
         staff_roles = uroles.get_all_staff_roles()
         check_roles = uroles.roles_check(
@@ -52,8 +52,81 @@ class TempWarnsCog(commands.Cog):
 
         roles_mention = ', '.join(role.mention for role in staff_roles)
 
+        if len(check_roles) == 0:
+            return await ctx.error(description = f'Эта команда доступна только для следующих ролей:\n {roles_mention}')
         
-        return await ctx.error(description = f'Эта команда доступна только для следующих ролей:\n {roles_mention}')
+        if isinstance(member, discord.Member):
+            if duration is None:
+                return await ctx.error(description = 'Вы не указали время устного во втором аргументе')
+        elif ctx.message.reference is not None:
+            if member is None:
+                return await ctx.error(description = 'Вы не указали время устного во первом аргументе')
+            duration = member
+            member = ctx.message.reference.resolved.author
+        else:
+            return await ctx.error(description = 'Участник не найден. Вы должны указать ник, упоминание или ID участника первым сообщением, либо ответить на сообщение того, кому нужно выдать устный')
+
+        if member.id == ctx.author.id:
+            return await ctx.error(description = 'Нельзя выдать устный себе')
+        
+        if member.bot:
+            return await ctx.error(description = 'Нельзя выдать устный боту')
+        
+        if membr.top_role.position >= ctx.author.top_role.position:
+            return await ctx.error(description = 'Нельзя выдать мьют участнику, который находится на одной роли с вами, либо с ролью выше чем у Вас')
+
+        member_warn = await self.db.get_warn(member = member)
+
+        if member_warn is not None:
+            return await ctx.error(description = 'У данного участника уже есть устный')
+        
+        error_fields = [
+                    discord.EmbedField(
+                     name = 'Пример, как правильно указывать время', 
+                      value = '`2ч` — 2 часа\n`30мин` — 30 минут\n`1д` — 1 день'
+                    ),
+                    discord.EmbedField(
+                        name = 'Доступные единицы измерения времени', 
+                        value = '**секунда** — с, сек, секунд\n**минута** — м, мин, минут\n**час** — ч, часов\n**день** — д, дня, дней'
+                    )
+                ]
+
+        warn_duration, warn_duration_type = get_duration(duration)
+
+        if not(warn_duration.isdigit()):
+            return await ctx.error(
+                description = 'Вы указали не положительное число перед единицей времени',
+                fields = error_fields
+            )
+        
+        warn_duration = int(warn_duration)
+
+        if warn_duration_type == 0:
+            return await ctx.error(
+                description = 'Вы указали неверный формат времени. Он должен начинаться с цифры, а заканчиваться единицей измерения времени.',
+                fields = error_fields
+            )
+        elif warn_duration_type == 2:
+            warn_duration *= 60
+        elif warn_duration_type == 3:
+            warn_duration *= 60 * 60
+        elif warn_duration_type == 4:
+            warn_duration *= 60 * 60 * 24
+        
+        warn_duration = datetime.timestamp(datetime.now()) + warn_duration
+        max_duration = datetime.timestamp(datetime.now()) + 1209602 
+                    
+        if warn_duration > max_duration:
+            return await ctx.error(description = 'Нельзя выдать устный длительностью больше 2 недель')
+        
+        await self.db.insert_warn(
+            author = ctx.author,
+            member = member,
+            duration = int(warn_duration)
+        )
+
+            
+
 
 
 def setup(bot):
--- utils/other.py
+++ utils/other.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime, timedelta
 
 def nc(arg: str) -> str:
     step, new_str = 0, ''
@@ -12,6 +13,33 @@ def nc(arg: str) -> str:
         step += 1
     
     return new_str[::-1]
+
+
+def get_duration(arg: str) -> str:
+    if number.endswith('с'):
+        return number[:-1], 1
+    elif number.endswith('сек'):
+        return number[:-3], 1
+    elif number.endswith('секунд'):
+        return number[:-6], 1
+    elif number.endswith('м'):
+        return number[:-1], 2
+    elif number.endswith('мин'):
+        return number[:-3], 2
+    elif number.endswith('минут'):
+        return number[:-5], 2
+    elif number.endswith('ч'):
+        return number[:-1], 3
+    elif number.endswith('часов'):
+        return number[:-5], 3
+    elif number.endswith('д'):
+        return number[:-1], 4
+    elif number.endswith('дней'):
+        return number[:-4], 4
+    elif number.endswith('дня'):
+        return number[:-3], 4
+    else:
+        return '1', 0
 
 
 async def auto_role(member: discord.Member, role: discord.Role) -> str:
