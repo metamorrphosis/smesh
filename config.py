dab574ce9327c892ae2f8b52f7c6b5b4ed16c963
metamorrphosis <104532312+metamorrphosis@users.noreply.github.com>
2022-10-22 00:48:09 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,12 @@
-token_main = "MTAxNTk4MzAwNjU0NjE0OTQyNw.GunmkN.6tvHYcmmMfMeSt8afqLO7tSL3Gn72cairEtzHM"
-cmd_prefix =  "s."
-mongodb_link = "mongodb+srv://bebroid:G4OkpALTJyAe2rlG@smeshcluster.hmfuskg.mongodb.net/?retryWrites=true&w=majority"
+# ————— MAIN —————
+token_main: str = "MTAxNTk4MzAwNjU0NjE0OTQyNw.GunmkN.6tvHYcmmMfMeSt8afqLO7tSL3Gn72cairEtzHM"
+cmd_prefix: str =  "s."
+mongodb_link: str = "mongodb+srv://bebroid:G4OkpALTJyAe2rlG@smeshcluster.hmfuskg.mongodb.net/?retryWrites=true&w=majority"
+
+# ————— CHANNELS —————
+tickets_category: int = 1008407518420148224
+tickets_channel: int = 1004832237872762980
+autoroles_channel: int = 1004655381806600222
+colors_channel: int = 1007102685658488842
+vacancies_channel: int = 1005221671432618045
+vacancies_answers_channel: int = 1007118729697562715
